a0ef1d5017d8eca8149f6a2e3b09e21eb9be18e7
Prakriti <prakritisingh14052008@gmail.com>
2026-09-02 09:52:46 +0530

    Create App4.js

---
+++ app/frontend/SIC/App4.js
@@ -0,0 +1,5 @@
+      <Route path="/faq" element={<FAQ />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/payment/success" element={<PaymentSuccess />} />
+        <Route path="/payment/cancel" element={<PaymentCancel />} />
+        <Route path="/login" element={<Login />} />
