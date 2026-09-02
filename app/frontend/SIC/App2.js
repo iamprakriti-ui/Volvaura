dff6174aba17c612e712e0a35512fc2b84813386
Prakriti <prakritisingh14052008@gmail.com>
2026-09-02 09:47:00 +0530

    Add App2.js with imports for static and creator pages

---
+++ app/frontend/SIC/App2.js
@@ -0,0 +1,4 @@
+import { HowItWorks, About, Contact, FAQ } from "@/pages/StaticPages";
+import { CreatorHome, NewProject, MyProjects, CreatorRequests, SavedEditors } from "@/pages/creator/CreatorPages";
+import { EditorHome, EditorPortfolio, EditorRequests, EditorSettings } from "@/pages/editor/EditorPages";
+import { Pricing, PaymentSuccess, PaymentCancel } from "@/pages/Payments";
